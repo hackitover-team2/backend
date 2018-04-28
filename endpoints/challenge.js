749c7de85055ab174622fabc67b122f9c7ce14cc
tony <tonygon@gmail.com>
2018-04-29 00:25:12 +0200

    Move telematix to external

--- endpoints/challenge.js
+++ endpoints/challenge.js
@@ -1,11 +1,9 @@
-var settings = require('../settings');
-
 // dependencies
 
 const express = require('express');
 const router = express.Router();
 const _ = require('underscore');
-const request = require('request');
+const telematix = require('../external/telematix');
 
 // models
 
@@ -13,35 +11,6 @@ var Challenge = require('../model/challenge');
 var User = require('../model/user');
 
 var createPayouts = require('../model/payouts').createPayouts;
-
-// external
-
-function getDriverScores(callback) {
-  var options = {
-    method: 'GET',
-    url: 'https://csv.telematics.tomtom.com/extern',
-    qs: {
-      lang: 'eng',
-      account: 'CM-Test',
-      username: 'API',
-      password: settings.telemetrics.password,
-      apikey: settings.telemetrics.token,
-      outputformat: 'json',
-      action: 'showOptiDriveIndicator',
-      range_pattern: 'w-3',
-    },
-  };
-
-  request(options, function (error, response, body) {
-    try {
-      var json = JSON.parse(body);
-      callback(null, json);
-    } catch (e) {
-      callback(e);
-    }
-
-  });
-}
 
 // Routes
 
@@ -70,13 +39,11 @@ router.post('/close/', function (req, res) {
     var property = challenge.goal.property;
     var optimum = challenge.goal.highOrLow;
 
-    getDriverScores(function (err, scores) {
+    telematix.getDriverScores(function (err, scores) {
       if (err) return req.statusCode(500).json(err);
 
       const compare = (a, b, comp) => (comp === 'low') ? a > b : a < b;
       var best = scores.reduce((prev, curr) => compare(prev[property], curr[property], optimum) ? prev : curr);
-
-      console.log(best);
 
       User.findOne({ telemetricsId: best.driverno }, function (err, user) {
         if (err) return res.status(500).send(err);
@@ -131,7 +98,7 @@ router.post('/leaderboard/', function (req, res) {
     var property = challenge.goal.property;
     var optimum = challenge.goal.highOrLow;
 
-    getDriverScores(function (err, scores) {
+    telematix.getDriverScores(function (err, scores) {
       if (err) return req.statusCode(500).json(err);
 
       var source = scores.map(s => Object.assign({}, _.pick(s, property, 'drivername')));
