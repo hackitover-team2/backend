158cf401edd276ee6d1c48f534bb360c9425258e
Dennis Schröder <dennis@wearsatie.com>
2018-04-28 15:36:23 +0200

    init challenge model

--- model/challenge.js
+++ model/challenge.js
@@ -3,7 +3,18 @@ var Schema = mongoose.Schema;
 
 var ChallengeSchema   = new Schema({
     name: String,
-    city: String
+    city: String,
+    description: String,
+    startDate: Date,
+    endDate: Date,
+    image: String,
+    goal: { 
+        property: String,
+        highOrLow: String
+    },
+    participants: Array,
+    price: String,
+    closed: { type: Boolean, default: false }
 });
 
 module.exports = mongoose.model('Challenge', ChallengeSchema);
